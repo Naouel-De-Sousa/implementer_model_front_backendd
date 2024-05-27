972c749174c38738b6f758446d784caf2327b62e
Naouel-De-Sousa <naouel31000dev@gmail.com>
2024-05-27 17:15:12 +0200

    csv path 27 mai

--- download_files.py
+++ download_files.py
@@ -7,8 +7,8 @@ def download_file(url, local_path):
         file.write(response.content)
 
 # URLs de vos fichiers sur GitHub (raw URLs)
-csv_url = 'https://github.com/Naouel-De-Sousa/implementer_model_front_backendd/blob/master/donn%C3%A9es_pour_model.csv'
-model_url = 'https://github.com/Naouel-De-Sousa/implementer_model_front_backendd/blob/master/models/mon_pipeline_complet.joblib'
+csv_url = 'https://github.com/Naouel-De-Sousa/implementer_model_front_backendd/raw/master/donn%C3%A9es_pour_model.csv'
+model_url = 'https://github.com/Naouel-De-Sousa/implementer_model_front_backendd/raw/master/models/mon_pipeline_complet.joblib'
 
 # Chemins locaux où les fichiers seront enregistrés
 csv_local_path = 'données_pour_model.csv'
