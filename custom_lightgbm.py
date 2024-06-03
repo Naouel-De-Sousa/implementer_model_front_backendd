2fcf8697572598ba27658f3d4e5da79290aa0899
Naouel-De-Sousa <naouel31000dev@gmail.com>
2024-06-03 15:28:19 +0200

    lightgbm error4  03 juin

--- custom_lightgbm.py
+++ custom_lightgbm.py
@@ -2,13 +2,8 @@ from lightgbm import LGBMClassifier, Booster as OriginalBooster
 import joblib
 
 class CustomBooster(OriginalBooster):
-    def __init__(self, model_file=None, model_str=None):
-        if model_file:
-            super().__init__(model_file=model_file)
-        elif model_str:
-            super().__init__(model_str=model_str)
-        else:
-            raise ValueError("Either model_file or model_str must be provided")
+    def __init__(self, booster):
+        self.__dict__.update(booster.__dict__)
 
     def predict(self, data, *args, **kwargs):
         return super().predict(data, *args, **kwargs)
@@ -16,12 +11,12 @@ class CustomBooster(OriginalBooster):
 class CustomLGBMClassifier(LGBMClassifier):
     def fit(self, *args, **kwargs):
         super().fit(*args, **kwargs)
-        self._Booster = CustomBooster(model_str=self.booster_.model_to_string())
+        self._Booster = CustomBooster(self.booster_)
         return self
 
 def wrap_booster(classifier):
     if hasattr(classifier, 'booster_'):
-        classifier._Booster = CustomBooster(model_str=classifier.booster_.model_to_string())
+        classifier._Booster = CustomBooster(classifier.booster_)
     return classifier
 
 def load_custom_pipeline(pipeline_path):
@@ -32,6 +27,4 @@ def load_custom_pipeline(pipeline_path):
             classifier = wrap_booster(classifier)
             pipeline.named_steps['classifier'] = classifier
     return pipeline
-
-
 
