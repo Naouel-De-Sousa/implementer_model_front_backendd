45e655eb491604fb9b3d9d64537dc22fa7afc34b
Naouel-De-Sousa <naouel31000dev@gmail.com>
2024-06-18 12:31:12 +0200

    error port heroku

--- run.sh
+++ run.sh
@@ -3,14 +3,10 @@
 # Run setup script for Streamlit configuration
 sh setup.sh
 
-# Detect OS and run the appropriate server
-if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]; then
-    # Unix-like OS, use gunicorn
-    gunicorn app:app &
-else
-    # Windows OS, use waitress
-    python -m waitress app:app &
-fi
+# Start the Flask server using Gunicorn
+gunicorn app:app &
 
-# Start the Streamlit server to serve your Streamlit app
+# Start the Streamlit server
 streamlit run front_end.py
+
+
