7df201d9726c7a7d1bad1fd6c35843083b885019
Naouel-De-Sousa <naouel31000dev@gmail.com>
2024-06-18 17:51:58 +0200

    port heroku error

--- app.py
+++ app.py
@@ -192,8 +192,8 @@ def get_all_client_info():
 
     return jsonify(data_dict)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
     
 
