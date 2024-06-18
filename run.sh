2cf3c134e10d0fa0950c086c3f1aec8f22b0c9e0
Naouel-De-Sousa <naouel31000dev@gmail.com>
2024-06-18 17:31:24 +0200

    port heroku error

--- run.sh
+++ run.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-Run setup script for Streamlit configuration
+# Run setup script for Streamlit configuration
 sh setup.sh
 
-Detect OS and run the appropriate server
+# Detect OS and run the appropriate server
 if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]; then
-# Unix-like OS, use gunicorn
-gunicorn app
-&
+    # Unix-like OS, use gunicorn
+    gunicorn app:app &
 else
-# Windows OS, use waitress
-python -m waitress app
-&
+    # Windows OS, use waitress
+    python -m waitress app:app &
 fi
 
-Start the Streamlit server to serve your Streamlit app
+# Start the Streamlit server to serve your Streamlit app
 streamlit run front_end.py
+
 
 
 
